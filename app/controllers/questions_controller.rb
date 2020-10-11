b5bf2f9cfcb58715957447f4c8f69e3fc83c7de4
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2020-10-11 16:49:09 -0500

    Refactor questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,25 +4,30 @@ class QuestionsController < ApplicationController
   before_action :check_validation
 
   def show
-    render json: question_presenter(Question.includes(:user, answers: [:user]).where(id: params[:id], private: false))
+    render json: question_presenter(question_query(params[:id]))
   end
 
   def index
-    render json: question_presenter(Question.includes(:user, answers: [:user]).where(private: false))
+    render json: question_presenter(question_query)
   end
 
   private
 
-    def check_validation
-      head :unauthorized unless Tenant.valid_key?(request.headers['HTTP_API_KEY'])
-    end
+  def check_validation
+    head :unauthorized unless Tenant.valid_key?(request.headers['HTTP_API_KEY'])
+  end
 
-    def question_presenter(questions)
-      output = []
-      questions.each do |q|
-        answers = q.answers.map { |a| a.as_json.merge(answerer_name: a.user.name, answerer_id: q.user_id) }
-        output << q.as_json.merge(asker_name: q.user.name, asker_id: q.user_id, answers: answers)
-      end
-      output
+  def question_query(id = nil)
+    where_method_params = id ? { private: false, id: id } : { private: false } 
+    Question.includes(:user, answers: [:user]).where(where_method_params)
+  end
+
+  def question_presenter(questions)
+    output = []
+    questions.each do |q|
+      answers = q.answers.map { |a| a.as_json.merge(answerer_name: a.user.name, answerer_id: q.user_id) }
+      output << q.as_json.merge(asker_name: q.user.name, asker_id: q.user_id, answers: answers)
     end
+    output
+  end
 end
